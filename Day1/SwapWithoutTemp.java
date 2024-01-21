a32b1af13335be7508c16df38f35bb29fd6d16d5
AdarshPandey-07 <116656875+AdarshPandey-07@users.noreply.github.com>
2024-01-21 22:27:35 +0530

    Create SwapWithoutTemp.java
    Enter Value of A
    56
    Enter Value of B
    54
    54
    56

---
+++ Day1/SwapWithoutTemp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SwapWithoutTemp {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+                System.out.println("Enter Value of A");
+                int a = sc.nextInt();
+                System.out.println("Enter Value of B");
+                int b = sc.nextInt();
+                
+                a = a+b;
+                b = a-b;
+                a= a-b;
+                
+                System.out.println(a);
+                System.out.println(b);
+                
+                
+    }
+}
