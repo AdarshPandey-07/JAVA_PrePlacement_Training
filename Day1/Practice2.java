1b0f3e3e5534e47c18beb4c580277ee760aae797
AdarshPandey-07 <116656875+AdarshPandey-07@users.noreply.github.com>
2024-01-21 22:19:11 +0530

    Create Practice2.java

---
+++ Day1/Practice2.java
@@ -0,0 +1,15 @@
+class Test
+{
+    static public void show()
+    {
+        System.out.println("Hi");
+    }
+}
+public class Solution1 {
+
+    public static void main(String[] args) {
+        
+        Test.show();
+    }
+    
+}
