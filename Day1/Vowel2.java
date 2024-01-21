831b050612bc29526c8b5767269f3898cf1a512d
AdarshPandey-07 <116656875+AdarshPandey-07@users.noreply.github.com>
2024-01-21 22:36:38 +0530

    Create Vowel2.java

---
+++ Day1/Vowel2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Vowel2 {
+
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter char");
+        char n = sc.next().charAt(0);
+        
+        switch (n){
+            case ('a'):
+                System.out.println("Vowel");
+                break;
+            case ('e'):
+                System.out.println("Vowel");
+                break;
+            case ('i'):
+                System.out.println("Vowel");
+                break;
+            case ('o'):
+                System.out.println("Vowel");
+                break;
+            case ('u'):
+                System.out.println("Vowel");
+                break;    
+            default:
+                System.out.println("Consonant");
+                break;
+        }
+        
+       
+    }
+}
