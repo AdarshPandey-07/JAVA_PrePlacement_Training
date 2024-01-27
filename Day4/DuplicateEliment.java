62fc83cdbcece92917cfd7590fc8d741fcb4ac23
Adarsh Pandey <116656875+AdarshPandey-07@users.noreply.github.com>
2024-01-28 00:37:55 +0530

    Create DuplicateEliment.java

---
+++ Day4/DuplicateEliment.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class DuplicateEliment {
+        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        
+        int arr[] = new int[len];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        int opr=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println("Repeat");
+                    opr++;
+                    break;}
+            }
+            if(opr>0)
+                break;
+        }
+    }      
+}
